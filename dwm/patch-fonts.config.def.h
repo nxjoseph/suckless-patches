ac63c4ea361a8988242e80cfebc266675d8f56d3
Yusuf <yusuf@desktop.home.lan>
2024-01-12 20:36:37 +0300

    adjust fonts

--- dwm/patch-fonts.config.def.h
+++ dwm/patch-fonts.config.def.h
@@ -1,13 +1,13 @@
---- config.def.h.orig	2023-12-18 15:34:24.534319000 +0300
-+++ config.def.h	2023-12-18 15:35:45.015316000 +0300
+--- config.def.h.orig	2024-01-12 20:30:36.458590000 +0300
++++ config.def.h	2024-01-12 20:31:14.410205000 +0300
 @@ -5,8 +5,8 @@
  static const unsigned int snap      = 32;       /* snap pixel */
  static const int showbar            = 1;        /* 0 means no bar */
  static const int topbar             = 1;        /* 0 means bottom bar */
 -static const char *fonts[]          = { "monospace:size=10" };
 -static const char dmenufont[]       = "monospace:size=10";
-+static const char *fonts[]          = { "Agave Nerd Font Mono:pixelsize=18:style=Bold" };
-+static const char dmenufont[]       = "Agave Nerd Font Mono:pixelsize=18:style=Bold";
++static const char *fonts[]          = { "Agave Nerd Font Mono:size=12" };
++static const char dmenufont[]       = "Agave Nerd Font Mono:size=12";
  static const char col_gray1[]       = "#222222";
  static const char col_gray2[]       = "#444444";
  static const char col_gray3[]       = "#bbbbbb";
